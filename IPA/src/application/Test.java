f68b29e6e1aab3724dd8eea0dbd21187d5138b1d
Raj <32778886+rk16449@users.noreply.github.com>
2018-05-05 19:48:42 +0100

    Added a test and questions

---
+++ IPA/src/application/Test.java
@@ -0,0 +1,17 @@
+package application;
+
+/*
+ * Test class that represents each test to be taken
+ */
+public class Test {
+
+	// Category of test
+	
+	// Timer of test
+	
+	// Questions within the test
+	
+	// Answers within the test
+	
+	// Score of the test
+}
