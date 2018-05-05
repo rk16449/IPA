09e53df7c0f50f9d5636ba9c931c40afcab35419
Raj <32778886+rk16449@users.noreply.github.com>
2018-05-05 20:16:28 +0100

    connected application layer question to button

--- IPA/src/controllersPackage/DashboardController.java
+++ IPA/src/controllersPackage/DashboardController.java
@@ -6,8 +6,13 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class DashboardController implements Initializable {
 
@@ -31,8 +36,15 @@ public class DashboardController implements Initializable {
 	protected void handleApplicationLayer(ActionEvent event) throws IOException {
 		System.out.println("Handle application layer button");
 		
-		// Start a new test
+		// Start a new test, pass application layer as the category
 		
-		
+		Parent parent = FXMLLoader.load(getClass().getResource("/fxmlPackage/stateQuestion.fxml"));
+		Scene scene = new Scene(parent);
+		Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		appStage.setScene(scene);
+		appStage.setTitle("IPA Program");
+		appStage.setWidth(944);
+		appStage.setHeight(600);
+		appStage.show();
 	}
 }
